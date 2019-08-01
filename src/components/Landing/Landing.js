c2b4b40a6f331e3e7b440c8102360e994d82d687
amiya-1998 <amiya.tripathy1998@gmail.com>
2019-08-01 22:41:48 +0530

    Change layout

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Landing extends Component {
   render() {
     return (
-      <div className='landing-container'>
+      <div className='landing-container data-container'>
         <div className='landing-card-wrapper'>
           <div className='landing-card'>Hello</div>
           <div className='landing-card'>Hello</div>
           <div className='landing-card'>Hello</div>
         </div>
-        <div className='landing-btn'>Get Started</div>
+        <div className='landing-btn'>
+          <Link to='/build'>Get Started</Link>
+        </div>
       </div>
     );
   }
