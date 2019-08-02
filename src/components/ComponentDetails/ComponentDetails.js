acda95c18024311ef1d632c1de21010971caee72
geekysrm <soumyacool2012@gmail.com>
2019-08-02 16:22:11 +0530

    add layout for component details

--- src/components/ComponentDetails/ComponentDetails.js
+++ src/components/ComponentDetails/ComponentDetails.js
@@ -1,7 +1,47 @@
 import React, { Component } from "react";
+import { Empty, List, Avatar, Icon } from "antd";
+
+import CompanyImage from "../../assets/images/companies/intel.png";
 
 export default class ComponentDetails extends Component {
   render() {
-    return <div className="component-details">Hello From Details</div>;
+    const listData = [
+      {
+        title: "Processor: i3",
+        description:
+          "Ant Design, a design language for background applications, is refined by Ant UED Team.",
+        cost: 10,
+        spr: 20,
+        cpr: 10,
+        image: CompanyImage,
+      },
+    ];
+    return (
+      <div className="component-details">
+        {/* <Empty description={<span>Please select an item</span>} /> */}
+        <List
+          itemLayout="vertical"
+          size="large"
+          dataSource={listData}
+          renderItem={item => (
+            <List.Item
+              key={item.title}
+              extra={
+                <>
+                  <img width={272} alt="logo" src={item.image} />
+                  <br />
+                  <b>Intel</b>
+                </>
+              }
+            >
+              <List.Item.Meta title={<span>{item.title}</span>} />
+              <h3>$ {item.cost} </h3> <br />
+              <h5>CPR: {item.cpr} </h5> <br />
+              <h5>SPR: {item.spr} </h5> <br />
+            </List.Item>
+          )}
+        />
+      </div>
+    );
   }
 }
