98190fa1441d4e610cfd6bb41228b1b7da842b9b
amiya-1998 <amiya.tripathy1998@gmail.com>
2019-08-02 22:15:32 +0530

    add styles to list cards

--- src/components/ComponentList/ComponentList.js
+++ src/components/ComponentList/ComponentList.js
@@ -7,43 +7,44 @@ export default class ComponentList extends Component {
         <div className="components-list-header">
           <h3>Components</h3>
         </div>
+        <hr />
         {this.props.list ? (
           <div className="components-list-content style-2">
-            <div className="components-list-card">
+            <div
+              className="components-list-card"
+              onClick={() => this.props.onClick("cpu")}
+            >
               {this.props.list.cpu.processor}
-              <button onClick={() => this.props.onClick("cpu")}>
-                View Details
-              </button>
             </div>
-            <div className="components-list-card">
+            <div
+              className="components-list-card"
+              onClick={() => this.props.onClick("gpu")}
+            >
               {this.props.list.gpu.name}
-              <button onClick={() => this.props.onClick("gpu")}>
-                View Details
-              </button>
             </div>
-            <div className="components-list-card">
+            <div
+              className="components-list-card"
+              onClick={() => this.props.onClick("hdd")}
+            >
               {this.props.list.hdd.size}
-              <button onClick={() => this.props.onClick("hdd")}>
-                View Details
-              </button>
             </div>
-            <div className="components-list-card">
+            <div
+              className="components-list-card"
+              onClick={() => this.props.onClick("ram")}
+            >
               {this.props.list.ram.size}
-              <button onClick={() => this.props.onClick("ram")}>
-                View Details
-              </button>
             </div>
-            <div className="components-list-card">
+            <div
+              className="components-list-card"
+              onClick={() => this.props.onClick("keyboard")}
+            >
               {this.props.list.keyboard.supplier}
-              <button onClick={() => this.props.onClick("keyboard")}>
-                View Details
-              </button>
             </div>
-            <div className="components-list-card">
+            <div
+              className="components-list-card"
+              onClick={() => this.props.onClick("mouse")}
+            >
               {this.props.list.mouse.supplier}
-              <button onClick={() => this.props.onClick("mouse")}>
-                View Details
-              </button>
             </div>
           </div>
         ) : null}
