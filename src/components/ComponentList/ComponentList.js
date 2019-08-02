339c7d00ae9669db1e20256e85870c58730fed4e
amiya-1998 <amiya.tripathy1998@gmail.com>
2019-08-02 16:24:27 +0530

    component list layout change

--- src/components/ComponentList/ComponentList.js
+++ src/components/ComponentList/ComponentList.js
@@ -2,6 +2,23 @@ import React, { Component } from "react";
 
 export default class ComponentList extends Component {
   render() {
-    return <div className="components-list">Hello from list</div>;
+    // const {cpu, gpu, ram, keyboard, mouse, screen, hdd} = this.props;
+    return (
+      <div className="components-list style-2">
+        <div className="components-list-header">
+          <h3>Components</h3>
+        </div>
+        <div className="components-list-content style-2">
+          <div className="components-list-card">List</div>
+          <div className="components-list-card">List</div>
+          <div className="components-list-card">List</div>
+          <div className="components-list-card">List</div>
+          <div className="components-list-card">List</div>
+          <div className="components-list-card">List</div>
+          <div className="components-list-card">List</div>
+          <div className="components-list-card">List</div>
+        </div>
+      </div>
+    );
   }
 }
